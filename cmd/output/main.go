267ba95bb2606e60b58da63fd098e1f06267db8c
catatsuy <catatsuy@catatsuy.org>
2025-08-30 13:34:28 +0900

    update random import to rand/v2 and fix sleep duration calculation

--- cmd/output/main.go
+++ cmd/output/main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
+	"math/rand/v2"
 	"time"
 )
 
 func init() {
 	flag.Parse()
-	rand.Seed(time.Now().Unix())
 }
 
 func main() {
 	for i := 0; i < 1000; i++ {
 		fmt.Printf("Welcome %d times\n", i)
 		// sleep 10ms-30ms
-		time.Sleep((time.Duration)(rand.Intn(3)+1) * 10 * time.Millisecond)
+		time.Sleep((time.Duration)(rand.IntN(3)+1) * 10 * time.Millisecond)
 	}
 }
